8bb284a961130f520bbfbae585872393b958cac6
Harri Rissanen <harri.rissanen@fns.fi>
2023-03-18 15:53:28 +0200

    Format bus timestamp in local tz and format

--- app/src/main/java/com/example/busesonthemap/MainActivity.kt
+++ app/src/main/java/com/example/busesonthemap/MainActivity.kt
@@ -21,6 +21,13 @@ import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.CustomZoomButtonsController
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
+import java.text.DateFormat
+import java.text.DateFormat.getDateTimeInstance
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class MainActivity : AppCompatActivity() {
     private lateinit var map: MapView
@@ -39,6 +46,22 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private lateinit var localDateTimeFormat: DateFormat
+    private lateinit var busDateParser: SimpleDateFormat
+
+    private fun getFormattedTimestamp(timestamp: String?): String {
+        if (timestamp == null || timestamp.isEmpty()) {
+            return "Unknown timestamp"
+        }
+        val parsedDate: Date
+        try {
+            parsedDate = busDateParser.parse(timestamp) as Date
+        } catch (exc: ParseException) {
+            return "Unknown timestamp"
+        }
+        return localDateTimeFormat.format(parsedDate)
+    }
+
     private fun pollForBusLineUpdates(): Job {
         return CoroutineScope(Dispatchers.Default).launch {
             while (true) {
@@ -50,28 +73,33 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun setBusMarkerInfo(busMarker: Marker, hslBus: HslBus) {
+        busMarker.title = hslBus.lineName
+        busMarker.snippet = "${hslBus.speedKph}<br>${getFormattedTimestamp(hslBus.timestamp)}"
+        busMarker.position = GeoPoint(hslBus.lat!!, hslBus.lon!!)
+    }
+
     private fun updateMarkers(busLines: ArrayList<HslBus>?) {
         busLines?.forEach { hslBus ->
             var busMarker: Marker? = busMarkers[hslBus.id]
             if (busMarker == null) {
                 busMarker = Marker(map)
                 busMarker.id = hslBus.id
-                busMarker.title = hslBus.lineName
-                busMarker.snippet = "${hslBus.speedKph}<br>${hslBus.timestamp}"
-                busMarker.position = GeoPoint(hslBus.lat!!, hslBus.lon!!)
                 map.overlays.add(busMarker)
                 busMarkers[hslBus.id] = busMarker
-            } else {
-                busMarker.title = hslBus.lineName
-                busMarker.snippet = "${hslBus.speedKph}<br>${hslBus.timestamp}"
-                busMarker.position = GeoPoint(hslBus.lat!!, hslBus.lon!!)
             }
+            setBusMarkerInfo(busMarker, hslBus)
             map.invalidate()
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
+        setContentView(R.layout.activity_main)
+
+        initBusDateTimeFormatter()
         initMap()
     }
 
@@ -87,10 +115,16 @@ class MainActivity : AppCompatActivity() {
         unbindService(hslMqttServiceConnection)
     }
 
-    private fun initMap() {
-        getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
-        setContentView(R.layout.activity_main)
+    private fun initBusDateTimeFormatter() {
+        localDateTimeFormat = getDateTimeInstance()
+        busDateParser = SimpleDateFormat(
+            "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",
+            Locale.getDefault(),
+        )
+        busDateParser.timeZone = TimeZone.getTimeZone("UTC")
+    }
 
+    private fun initMap() {
         map = findViewById(R.id.map)
         map.setTileSource(TileSourceFactory.MAPNIK)
         map.setMultiTouchControls(true)
